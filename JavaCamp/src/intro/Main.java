c0e1b04b51e1cbc541dce58c701cb59e47335ba7
Kerem Candangil <keremc4306@gmail.com>
2021-04-22 00:11:08 +0300

    1. kamp günü sonu

--- JavaCamp/src/intro/Main.java
+++ JavaCamp/src/intro/Main.java
@@ -59,5 +59,23 @@ public class Main {
 		for(int i = 0; i < krediler.length; i++) {
 			System.out.println(krediler[i]);
 		}
+		
+		int sayi1 = 10;
+		int sayi2 = 20;
+		sayi1 = sayi2;
+		sayi2 = 100;
+		System.out.println(sayi1);
+		
+		int[] sayilar1 = {1,2,3,4,5};
+		int[] sayilar2 = {10,20,30,40,50};
+		sayilar1 = sayilar2;
+		sayilar2[0] = 100;
+		System.out.println(sayilar1[0]);
+		
+		String sehir1 = "Ankara";
+		String sehir2 = "Ýstanbul";
+		sehir1 = sehir2;
+		sehir2 = "Ýzmir";
+		System.out.println(sehir1);
 	}
 }
