79f20a77d431d047d8c371b2eca86461a40cb676
Kerem Candangil <keremc4306@gmail.com>
2021-04-24 23:19:34 +0300

    2. gün commit 2

--- JavaCamp/src/oopIntro/Product.java
+++ JavaCamp/src/oopIntro/Product.java
@@ -1,6 +1,19 @@
 package oopIntro;
 
 public class Product {
+	
+	//constructor
+	public Product() {
+		System.out.println("Ben çalýþtým");
+	}
+	
+	public Product(int id, String name, double unitPrice, String detail) {
+		this.id = id;
+		this.name = name;
+		this.unitPrice = unitPrice;
+		this.detail = detail;
+	}
+	
 	String name;
 	double unitPrice;
 	String detail;
