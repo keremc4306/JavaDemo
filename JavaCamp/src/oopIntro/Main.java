07b711468d4520f05deed0fbceabf1d1b6c97722
Kerem Candangil <keremc4306@gmail.com>
2021-04-24 22:21:15 +0300

    2. gün commit 1

---
+++ JavaCamp/src/oopIntro/Main.java
@@ -0,0 +1,19 @@
+package oopIntro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Product product1 = new Product(); //referans oluþturma, instance
+		product1.id = 1;
+		product1.name = "Lenovo V14";
+		product1.unitPrice = 15000;
+		product1.detail = "16 GB Ram";
+		
+		
+		
+		
+		Product[] products = {product1};
+		
+		System.out.println(products.length);
+	}
+}
---
+++ JavaCamp/src/oopIntro/Product.java
@@ -0,0 +1,8 @@
+package oopIntro;
+
+public class Product {
+	String name;
+	double unitPrice;
+	String detail;
+	int id;
+}
