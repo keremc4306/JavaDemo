e374fd50a4ea9b777be0617d122a06da9255756c
Kerem Candangil <keremc4306@gmail.com>
2021-04-25 21:28:09 +0300

    1. gün 3. ödev 6-7-8. videoların uygulamaları

--- JavaCamp/src/javademos/Main.java
+++ JavaCamp/src/javademos/Main.java
@@ -3,7 +3,33 @@ package javademos;
 public class Main {
 
 	public static void main(String[] args) {
-		//Merhaba Java uygulamasý
+		//Merhaba Java uygulamasý - 5. video
+		/*
 		System.out.println("Merhaba Java");
+		
+		System.out.println("*************************************************************");
+		*/
+		
+		
+		//Deðiþkenler - 6. video
+		//reusability: Tekrar tekrar kullanýlabilirlik
+		/*
+		int ogrenciSayisi = 12; //int: Tam sayý türünde
+		String mesaj = "Öðrenci Sayýsý"; //string: metinsel ifade
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öðrenci sayým: " + ogrenciSayisi);
+		System.out.println("Öðrenci sayým: " + ogrenciSayisi);
+		System.out.println("Öðrenci sayým: " + ogrenciSayisi);
+		*/
+		
+		//Veri tipleri - 7. video
+		/*
+		int sayi = 12;
+		byte sayi2 = 125; //-128 ile 127 arasýnda deðer alýr.
+		char karakter = 'a'; //Tek karakter tutmak istediðimizde kullanýrýz. Çok yoðun kullanmayýz.
+		String sehir = "Ankara"; //Ankara kelimesi içinde 6 harfli olan dizi
+		boolean dogruMu = true; //boolean oldukça yoðun kullanýlýr.
+		*/
 	}
 }
