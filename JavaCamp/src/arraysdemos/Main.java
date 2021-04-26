5d9b4b3a2aa5b72028cf53ba6deca802f557c1c3
Kerem Candangil <keremc4306@gmail.com>
2021-04-26 22:54:43 +0300

    Dizilerle çalışma

---
+++ JavaCamp/src/arraysdemos/Main.java
@@ -0,0 +1,36 @@
+package arraysdemos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		//Dizilerle çalýþma
+		String ogrenci1 = "Kerem";
+		String ogrenci2 = "Mertcan";
+		String ogrenci3 = "Edip";
+		String ogrenci4 = "Serdar";
+		
+		System.out.println(ogrenci1);
+		System.out.println(ogrenci2);
+		System.out.println(ogrenci3);
+		System.out.println(ogrenci4);
+		
+		System.out.println("------------------------------");
+		
+		String[] ogrenciler = new String[3];
+		ogrenciler[0] = "Kerem"; //1. eleman
+		ogrenciler[1] = "Edip"; //2. eleman
+		ogrenciler[2] = "Serdar"; //3. eleman
+		
+		for(int i = 0; i < ogrenciler.length; i++) {
+			System.out.println(ogrenciler[i]); //ogrenciler'in i. elemanýný ekrana yazdýrma
+		}
+		
+		System.out.println("------------------------------");
+		
+		for(String ogrenci:ogrenciler) {
+			System.out.println(ogrenci);
+		}
+		*/
+	}
+}
