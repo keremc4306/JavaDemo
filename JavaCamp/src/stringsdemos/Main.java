bc03b79fa2540491bb62fae76d09b4900b37ce42
Kerem Candangil <keremc4306@gmail.com>
2021-04-28 14:35:47 +0300

    1. gün 3. ödev 20. videoya kadar uygulamalar yapıldı

--- JavaCamp/src/stringsdemos/Main.java
+++ JavaCamp/src/stringsdemos/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		String message = "Bugün hava çok güzel";
 		System.out.println(message);
 		
+		/*
 		//length() fonksiyonu bize eleman sayýsýný verecektir.
 		System.out.println("Eleman sayýsý = " + message.length());
 		
@@ -22,6 +23,25 @@ public class Main {
 		
 		System.out.println(message.indexOf('a')); // Ýlk bulduðu a'da aramayý bitirir.
 		System.out.println(message.indexOf("av")); //mesajýn içinde av var mý ?
-		System.out.println(message.lastIndexOf("a")); // mesajýn içinde en son ki "a"nýn indisi verir 
+		System.out.println(message.lastIndexOf("a")); // mesajýn içinde en son ki "a"nýn indisi verir.
+		*/
+		
+		System.out.println(message.replace(' ', '-')); //replace metodu aralarýnda boþluk gördüðü yere - koydu.
+		
+		//substring metodu: Bir metnin içerisinden parça alýnýr.
+		System.out.println(message.substring(2)); //2. indexinden itibaren mesajý yazar
+		
+		//substring metodu diðer kullanýmý: 2. indexten istediðimiz indexe kadar keser.
+		System.out.println(message.substring(2, 4)); //2'den baþla 4'e kadar
+		System.out.println(message.substring(2, 5)); //2'den baþla 5'e kadar (n harfinden sonra boþluðu dahil etmeyecek.)
+		
+		//split fonksiyonu: Bir metni belli bir karakter veya karakter dizisini dikkate alarak parçalamaya yarar.
+		for(String kelime:message.split(" ")) {
+			System.out.println(kelime);
+		}
+		
+		System.out.println(message.toLowerCase()); //kelimelerin bütün harflerini küçük harfe çevirecek
+		System.out.println(message.toUpperCase()); //kelimelerin bütün harflerini büyük harfe çevirecek
+		System.out.println(message.trim()); //mesajýn baþýndaki ve sonundaki boþluklarý atýyor
 	}
 }
