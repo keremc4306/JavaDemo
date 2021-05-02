fb7484ba29476548708378a9b5c8f7a0b11beb83
Kerem Candangil <keremc4306@gmail.com>
2021-05-02 19:14:14 +0300

    3. gün tekrar 2

---
+++ JavaCamp/src/inheritance/Main.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+		IndividualCustomer kerem = new IndividualCustomer();
+		
+		CorporateCustomer hepsiBurada = new CorporateCustomer();
+		
+	}
+
+}
