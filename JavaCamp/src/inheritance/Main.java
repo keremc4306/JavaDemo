5ec69165576dd5cc53855523ec46a47c7c18659a
Kerem Candangil <keremc4306@gmail.com>
2021-05-02 22:48:55 +0300

    3. gün tekrar 3

--- JavaCamp/src/inheritance/Main.java
+++ JavaCamp/src/inheritance/Main.java
@@ -3,10 +3,24 @@ package inheritance;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		IndividualCustomer kerem = new IndividualCustomer();
+		kerem.customerNumber  = "25648";
 		
 		CorporateCustomer hepsiBurada = new CorporateCustomer();
+		hepsiBurada.customerNumber = "52616";
 		
+		
+		SyndicateCustomer abc = new SyndicateCustomer();
+		abc.customerNumber = "99999";
+		
+		CustomerManager customerManager = new CustomerManager();
+		
+		Customer[] customers = {kerem, hepsiBurada, abc}; // array oluþturma
+		//customerManager.add(hepsiBurada);
+		//customerManager.add(kerem);
+		//customerManager.add(abc);
+		customerManager.addMultiple(customers);
 	}
 
 }
