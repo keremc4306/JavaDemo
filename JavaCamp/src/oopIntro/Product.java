c1c626337661c23b287fa114df253357110d8ec8
Kerem Candangil <keremc4306@gmail.com>
2021-05-02 18:03:46 +0300

    3. gün tekrar 1

--- JavaCamp/src/oopIntro/Product.java
+++ JavaCamp/src/oopIntro/Product.java
@@ -2,21 +2,72 @@ package oopIntro;
 
 public class Product {
 	
-	//constructor
+	//encapsulation-kapsülleme
+	//final: sadece constructorda set edilebilir demek
+	//private: özel demek, sadece bu sýnýfýn içinde kullanýlabilir demek
+	private int id;
+	private String name;
+	private double unitPrice;
+	private String detail;
+	private double discount;
+	
 	public Product() {
-		System.out.println("Ben çalýþtým");
+		
 	}
 	
-	public Product(int id, String name, double unitPrice, String detail) {
-		this();
+	public Product(int id, String name, double unitPrice, 
+			String detail, double discount) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.unitPrice = unitPrice;
 		this.detail = detail;
+		this.discount = discount;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+
+	public double getUnitPriceAfterDiscount() {
+		return this.unitPrice-(this.unitPrice * this.discount / 100);
 	}
 	
-	String name;
-	double unitPrice;
-	String detail;
-	int id;
+	
 }
