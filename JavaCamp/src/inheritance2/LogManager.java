a72c8126720c28af76b28c232e9e2d68e8b8a633
Kerem Candangil <keremc4306@gmail.com>
2021-05-03 10:22:49 +0300

    3. gün tekrar yapıldı loglama işlemleri yapıldı

---
+++ JavaCamp/src/inheritance2/LogManager.java
@@ -0,0 +1,17 @@
+package inheritance2;
+
+public class LogManager {
+	public void log(int logType) {
+		if(logType == 1) {
+			System.out.println("Veritabanýna loglandý");
+		}
+		
+		else if(logType == 2) {
+			System.out.println("Dosyaya loglandý");
+		}
+		
+		else {
+			System.out.println("Email gönderildi");
+		}
+	}
+}
---
+++ JavaCamp/src/inheritance2/Main.java
@@ -0,0 +1,12 @@
+package inheritance2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CustomerManager customerManager = new CustomerManager();
+		//customerManager.add(new DatabaseLogger());
+		//customerManager.add(new FileLogger());
+		customerManager.add(new EmailLogger());
+	}
+
+}
