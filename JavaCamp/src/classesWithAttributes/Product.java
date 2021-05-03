985c2941373ae2d667d4df3562d6a33f370ab647
Kerem Candangil <keremc4306@gmail.com>
2021-05-03 10:38:17 +0300

    Field ve attribute ile çalışma - 32. video

---
+++ JavaCamp/src/classesWithAttributes/Product.java
@@ -0,0 +1,10 @@
+package classesWithAttributes;
+
+public class Product {
+	//attribure | field
+	int id; //id: Ürünü tanýmlayan eþsiz deðer
+	String name;
+	String description;
+	double price;
+	int stockAmount;
+}
